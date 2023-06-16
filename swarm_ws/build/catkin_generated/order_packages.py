0e4d9ce0af66fb0c14c6169ca84febac3d891b47
SkateWarp <Anyeloalvarez@gmail.com>
2023-06-16 14:23:01 -0400

    empty package for testing

---
+++ swarm_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/anyelo/RobotSwarm/swarm_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/anyelo/ROS_CRP/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/anyelo/ROS_CRP/catkin_ws/devel;/opt/ros/noetic' != '' else []
