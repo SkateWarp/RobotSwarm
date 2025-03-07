1198a0ebc2f28cc39940580435cba1e68c7b0b13
Félix Alejandro Guzmán <felixalejandro@outlook.com>
2025-03-06 20:48:26 -0400

    add command to send

--- SwarmFrontend/src/app/main/apps/GeeTS/Realtime/RealtimeConfigList.js
+++ SwarmFrontend/src/app/main/apps/GeeTS/Realtime/RealtimeConfigList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import SendIcon from "@mui/icons-material/Send";
 import {
     RosConnection,
     TopicListProvider,
@@ -9,23 +9,81 @@ import {
     useServiceList,
     useTopicList,
 } from "rosreact";
+import { InputAdornment, InputLabel, OutlinedInput } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import axios from "axios";
 import PauseButton from "./buttons/PauseButton";
 import GenericServiceButton from "./buttons/GenericServiceButton";
+import singletonInstance from "../../../../services/SignalRService/signalRConnectionService";
+import { URL } from "../../../../constants/constants";
+import jwtService from "../../../../services/jwtService";
+
+const defaultTopics = [
+
+    "/robot/:id/sensor_data",
+    "/robot/:id/task/start",
+    "/robot/:id/status",
+
+];
 
 function RealtimeConfigList() {
-    const dispatch = useDispatch();
+    const [robots, setRobot] = useState([]);
+    const [topics, setTopics] = useState([]);
+    const [selectedTopic, setSelectedTopic] = useState("");
+    const [command, setCommand] = useState("");
     const [trigger, setTrigger] = useState(false);
-    const searchText = useSelector(({ realtimeConfigApp }) => realtimeConfigApp.config.searchText);
-
+    const connection = singletonInstance.createConnectionBuilder();
     useEffect(() => {
         setTimeout(() => {
             setTrigger(!trigger);
         }, 3000);
     }, [trigger]);
 
+    useEffect(() => {
+        axios
+            .get(`${URL}/Robots`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${jwtService.getAccessToken()}`,
+                },
+            })
+            .then((value) => {
+                const { data } = value;
+                setRobot(data);
+                const paths = [data.map((robot) => defaultTopics.map((t) => t.replace(":id", robot.id)))];
+                setTopics(paths);
+                console.log(topics);
+            });
+    }, []);
+
+    function onSend(e) {
+        console.log("onSend", e.target.value, command);
+        // connection.send();
+    }
+
     return (
-        <div className="flex flex-1 items-center justify-center h-full">
-            <RosConnection url="https://robot.zerav.la/hubs/robot" autoConnect >
+        <div className="flex flex-1 flex-col items-center h-full">
+            <div className="p-2">
+                <div className="w-full">
+                    <InputLabel htmlFor="outlined-adornment-password">Comando</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type="text"
+                        className="w-full"
+                        value={command}
+                        onChange={(e) => setCommand(e.target.value)}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton onClick={onSend} edge="end">
+                                    <SendIcon />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Comando"
+                    />
+                </div>
+            </div>
+            <RosConnection url="https://robot.zerav.la/hubs/robot" autoConnect>
                 <PauseButton />
                 <GenericServiceButton topicName="/gazebo/reset_simulation" text="Reset Simulation" />
                 <GenericServiceButton topicName="/gazebo/reset_world" text="Reset World" />
