3a9e36c78a52536d6edc7a0cd9ed52bad003bfef
Félix Alejandro Guzmán <felixalejandro@outlook.com>
2025-03-27 19:53:41 -0400

    use wss

--- SwarmFrontend/src/app/main/apps/GeeTS/Realtime/RealtimeConfigList.js
+++ SwarmFrontend/src/app/main/apps/GeeTS/Realtime/RealtimeConfigList.js
@@ -259,7 +259,7 @@ function RealtimeConfigList() {
                     />
                 </div>
             </div>
-            <VncViewer url="ws://186.7.92.67:6080" username="rs" password="123456789" />
+            <VncViewer url="wss://186.7.92.67:6080" username="rs" password="123456789" />
         </div>
     );
 }
