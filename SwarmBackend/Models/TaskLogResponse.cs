f8f5759cf0da553b98c61881ad616fde08692e0d
Félix Alejandro Guzmán <code@felixalejandro.com>
2025-05-01 19:18:54 -0400

    Agregar lista de Robots a TaskLogResponse y actualizar método From para incluirla

--- SwarmBackend/Models/TaskLogResponse.cs
+++ SwarmBackend/Models/TaskLogResponse.cs
@@ -1,14 +1,15 @@
+using System.Linq;
 using System.Text.Json;
 using SwarmBackend.Entities;
 
 namespace SwarmBackend.Models;
 
 public record TaskLogResponse(int Id, DateTime DateCreated, DateTime? DateFinished, int TaskTemplateId,
-    TaskTemplateResponse? TaskTemplate, List<int> RobotIds, JsonDocument Parameters, DateTime? DateCancelled)
+    TaskTemplateResponse? TaskTemplate, List<int> RobotIds, List<RobotResponse> Robots, JsonDocument Parameters, DateTime? DateCancelled)
 {
     public static TaskLogResponse From(TaskLog taskLog) => new(taskLog.Id, taskLog.DateCreated, taskLog.DateFinished,
         taskLog.TaskTemplateId, taskLog.TaskTemplate != null ? TaskTemplateResponse.From(taskLog.TaskTemplate) : null,
-        [.. taskLog.Robots.Select(x => x.Id)], taskLog.Parameters, taskLog.DateCancelled);
+        [.. taskLog.Robots.Select(x => x.Id)], [.. taskLog.Robots.Select(x => RobotResponse.From(x))], taskLog.Parameters, taskLog.DateCancelled);
 }
 
 public record TaskLogRequest(int TaskTemplateId, List<int> RobotIds, JsonElement Parameters);
