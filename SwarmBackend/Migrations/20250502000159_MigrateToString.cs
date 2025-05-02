ee5fe40f9facfa1be86e3a99ee60b10359ecf9ef
Félix Alejandro Guzmán <code@felixalejandro.com>
2025-05-01 20:02:17 -0400

    Actualizar tipo de dato de 'Value' en 'SensorReading' a string en las migraciones y el modelo de contexto

---
+++ SwarmBackend/Migrations/20250502000159_MigrateToString.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwarmBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrateToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "SensorReadings",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Value",
+                table: "SensorReadings",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
